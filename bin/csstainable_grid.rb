4a4593c75c0d08013c6df97dad4a7e06e6267152
wardpenney <ward.penney@gmail.com>
2014-01-05 14:09:34 -0500

    fix command line args

--- bin/csstainable_grid.rb
+++ bin/csstainable_grid.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'csstainable_grid'
-puts "My executable works! #{ARGV.join(' ')}"
-# puts "File: #{__FILE__}"
-# p = File.join(File.expand_path("../../.", __FILE__), 'lib', 'csstainable_grid', 'csstainable_generate.rb')
-# puts p
+
+p = File.join(File.expand_path("../../.", __FILE__), 'lib', 'csstainable_grid', 'csstainable_generate.rb')
 
 cmd = "ruby #{p} #{ARGV.join(" ")}"
 
